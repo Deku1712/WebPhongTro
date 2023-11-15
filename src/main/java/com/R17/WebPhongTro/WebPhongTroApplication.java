241005913e6b425b96676a9c6be5fc5a532745d0
Deku1712 <tamtvnde160577@fpt.edu.vn>
2023-11-15 14:17:29 +0700

    first commit

---
+++ src/main/java/com/R17/WebPhongTro/WebPhongTroApplication.java
@@ -0,0 +1,13 @@
+package com.R17.WebPhongTro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPhongTroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPhongTroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/R17/WebPhongTro/WebPhongTroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.R17.WebPhongTro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPhongTroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
